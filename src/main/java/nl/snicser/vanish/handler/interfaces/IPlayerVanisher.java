cb3cd22490dc61cb5b3f3c3b5f0d700da606b38a
Snicserr <flavio03@outlook.com>
2020-08-04 20:07:56 +0200

    Delete IPlayerVanisher.java

--- src/main/java/nl/snicser/vanish/handler/interfaces/IPlayerVanisher.java
+++
@@ -1,9 +0,0 @@
-package nl.snicser.vanish.handler.interfaces;
-
-import org.bukkit.entity.Player;
-
-public interface IPlayerVanisher {
-
-    boolean vanish(Player players);
-    boolean unvanish(Player player);
-}
